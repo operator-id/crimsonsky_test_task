00216c1be89a76c1bd8d0fedd2591d0f221e5587
operator-id <sergiu.slanina97@gmail.com>
2023-08-18 20:08:36 +0300

    project commit

---
+++ Assets/Content/Scripts/PlayerCharacter.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class PlayerCharacter : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rigidBody;
+        [SerializeField] private CircleCollider2D circleCollider;
+        [SerializeField] private LayerMask impassableLayer;
+
+        private bool _movesToTarget;
+        private Vector2 _destination;
+        private float _moveSpeed;
+        private Vector2 _moveDirection;
+
+
+        public void SetDestination(Vector2 destination, float moveSpeed)
+        {
+            _movesToTarget = true;
+            _moveDirection = (destination - rigidBody.position).normalized;
+            _moveSpeed = moveSpeed;
+            _destination = destination;
+        }
+
+        public void MoveWithIncrement(Vector2 increment)
+        {
+            _movesToTarget = false;
+            Move(increment);
+        }
+
+        public void DoFixedUpdate()
+        {
+            MoveToDestination();
+        }
+
+        private void MoveToDestination()
+        {
+            if (!_movesToTarget)
+            {
+                return;
+            }
+
+            if (Vector2.Distance(rigidBody.position, _destination) < 0.1F)
+            {
+                _movesToTarget = false;
+                return;
+            }
+
+            var moveIncrement = _moveDirection * (_moveSpeed * Time.fixedDeltaTime);
+
+            var hasCollided = Physics2D.OverlapCircle(rigidBody.position + moveIncrement, circleCollider.radius,
+                impassableLayer) != null;
+            if (hasCollided)
+            {
+                _movesToTarget = false;
+                return;
+            }
+
+            Move(moveIncrement);
+        }
+
+        private void Move(Vector2 increment)
+        {
+            rigidBody.MovePosition(rigidBody.position + increment);
+        }
+    }
+}
---
+++ Assets/Content/Scripts/PlayerController.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private PlayerCharacter character;
+        [SerializeField] private float moveSpeed = 1F;
+        [Space] [SerializeField] private Camera raycastCamera;
+        [SerializeField] private LayerMask impassableLayer;
+        [SerializeField] private LayerMask validMoveLayer;
+
+        private Vector2 _moveInput;
+        private bool _queuedMoveOrder;
+
+        private void Update()
+        {
+            HandleInput();
+        }
+
+        private void FixedUpdate()
+        {
+            if (_queuedMoveOrder)
+            {
+                var increment = _moveInput * (moveSpeed * Time.fixedDeltaTime);
+                character.MoveWithIncrement(increment);
+            }
+
+            character.DoFixedUpdate();
+        }
+
+        private void HandleInput()
+        {
+            if (TryGetMoveDestinationInput(out var destination))
+            {
+                character.SetDestination(destination, moveSpeed);
+                return;
+            }
+
+            _queuedMoveOrder = TryGetMoveDirectionInput(out _moveInput);
+        }
+
+        private bool TryGetMoveDestinationInput(out Vector3 position)
+        {
+            position = default;
+            var didClick = Input.GetMouseButtonUp(0);
+            if (!didClick)
+            {
+                return false;
+            }
+
+            var origin = raycastCamera.ScreenToWorldPoint(Input.mousePosition);
+            var results = new Collider2D[1];
+            var hitCount = Physics2D.OverlapPoint(origin, new ContactFilter2D
+            {
+                layerMask = validMoveLayer | impassableLayer
+            }, results);
+
+            if (hitCount == 0)
+            {
+                return false;
+            }
+
+            if (1 << results[0].gameObject.layer == validMoveLayer)
+            {
+                position = origin;
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool TryGetMoveDirectionInput(out Vector2 input)
+        {
+            input = Vector2.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                input.y += 1;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                input.x -= 1;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                input.y -= 1;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                input.x += 1;
+            }
+
+            return input != default;
+        }
+    }
+}
